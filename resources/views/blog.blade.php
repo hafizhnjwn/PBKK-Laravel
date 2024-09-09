1889b223f46923bdbe6e5817172567b06dfe4def
Hafizh <hafizhnjwn@gmail.com>
2024-09-09 14:23:56 +0700

    awal

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <article>
+        <h2>Salma Samsung</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam impedit ipsum similique ducimus quod, amet perferendis quidem totam illum nihil debitis consequatur inventore dolorem nobis quam expedita. Eaque, mollitia voluptate.
+        </p>
+    </article>
+
+    <article>
+        <h2>Rayzord</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio libero atque magnam aliquam veritatis unde, quisquam sed nam mollitia at tenetur possimus ad eum odio porro temporibus vitae voluptate sunt!
+        </p>
+    </article>
+    
+</body>
+</html>

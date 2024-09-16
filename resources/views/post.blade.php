6c2738a70867cea63a38a0ff975a7056c5a1c617
Hafizh <hafizhnjwn@gmail.com>
2024-09-16 20:21:22 +0700

    done view data

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  
+  <article class= "py-8 max-w-screen-md ">
+      <h3 class= "mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h3>
+    <div class= "text-base text-gray-500">
+      <a href="#">{{$post['author']}}</a> | 09 Juni 2004
+    </div>
+    <p class="my-4 font-light">{{ ($post['body']) }}
+      </p>
+      <a href="/posts" class="text-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
+    </article>  
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,55 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('welcome', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' =>[
+        [
+            'id' => 1,
+            'slug' => 'salma-samsung',
+            'title' => 'Salma Samsung',
+            'author' => 'Muhammad Hafizh Najwan Noor',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam impedit ipsum similique ducimus quod, amet perferendis quidem totam illum nihil debitis consequatur inventore dolorem nobis quam expedita. Eaque, mollitia voluptate.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'rayzord',
+            'title' => 'Rayzord',
+            'author' => 'Neva Talitha Desian Noor',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe aperiam sunt, error obcaecati tempora fuga quod deleniti sint, temporibus, ipsam maiores non harum ad quo sequi cupiditate? Totam, maiores vel. '
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'salma-samsung',
+            'title' => 'Salma Samsung',
+            'author' => 'Muhammad Hafizh Najwan Noor',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam impedit ipsum similique ducimus quod, amet perferendis quidem totam illum nihil debitis consequatur inventore dolorem nobis quam expedita. Eaque, mollitia voluptate.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'rayzord',
+            'title' => 'Rayzord',
+            'author' => 'Neva Talitha Desian Noor',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe aperiam sunt, error obcaecati tempora fuga quod deleniti sint, temporibus, ipsam maiores non harum ad quo sequi cupiditate? Totam, maiores vel. '
+        ]
+
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {

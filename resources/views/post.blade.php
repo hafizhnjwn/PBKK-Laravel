bdaa3865d4d5fbd005074dbff4dd228f69fac35e
Hafizh <hafizhnjwn@gmail.com>
2024-09-30 15:26:48 +0700

    done Seeder & fix view pointer

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
   <x-slot:title>{{ $title }}</x-slot:title>
   
   <article class= "py-8 max-w-screen-md ">
-      <h3 class= "mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h3>
+      <h3 class= "mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post->title }}</h3>
     <div>
       By
       <a href="/authors/{{$post->author->username}}" class="hover:underline text-base text-gray-500">{{$post->author->name}}</a> 
@@ -10,7 +10,7 @@
       <a href="/categories/{{$post->category->slug}}" class="hover:underline text-base text-gray-500">{{$post->category->name}}</a>
       | {{ $post->created_at->format('j F Y')}}
     </div>
-    <p class="my-4 font-light">{{ ($post['body']) }}
+    <p class="my-4 font-light">{{ $post->body }}
       </p>
       <a href="/posts" class="text-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
     </article>  
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
   @foreach ($posts as $post)
   <article class= "py-8 max-w-screen-md border-b border-gray-300">
     <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-      <h3 class= "mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h3>
+      <h3 class= "mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post->title }}</h3>
     </a>
     <div>
       By
@@ -13,7 +13,7 @@
       <a href="/categories/{{$post->category->slug}}" class="hover:underline text-base text-gray-500">{{$post->category->name}}</a>
       | {{ $post->created_at->format('j F Y')}}
     </div>
-    <p class="my-4 font-light">{{Str::limit($post['body'])}}
+    <p class="my-4 font-light">{{Str::limit($post->body)}}
       </p>
       <a href="/posts/{{ $post['slug'] }}" class="text-medium text-blue-500 hover:underline">Read more &raquo;</a>
     </article>  
